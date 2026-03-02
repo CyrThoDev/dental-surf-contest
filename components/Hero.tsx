4c14917d3251f9356c6ff9424a42707569478aa5
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2026-03-02 21:43:00 +0100

    changement affiche

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
 <section className="w-full">
   <Image
-    src="/dentalsurfcontestaffiche-2026.png"
+    src="/dentalsurfcontest2026.png"
     alt="Dental Surf Contest 2026"
     width={1200}
     height={1800}
